eebac923aeaab17bf2d38911d11c33570c476743
Max Sprauer <max-github@sprauer.org>
2018-12-03 22:14:22 -0500

    Day 5: This one was not too bad.

---
+++ 05_A_Maze_of_Twisty_Trampolines/jumps.php
@@ -0,0 +1,32 @@
+<?php
+
+$offsets = file_get_contents('input.txt');
+$offsets = explode("\n", $offsets);
+
+print_r($offsets);
+
+$offset = 0;
+$steps = 0;
+do {
+    $steps++;
+
+    $oldOffset = $offset;
+
+    // Jump to new offset
+    $offset += $offsets[$offset];
+
+    // Part 1
+    // Increment old offset
+    // $offsets[$oldOffset]++;
+
+    // Part 2
+    if ($offsets[$oldOffset] >= 3) {
+        $offsets[$oldOffset]--;    
+    } else {
+        // Increment old offset
+        $offsets[$oldOffset]++;
+    }
+
+} while ($offset < count($offsets));
+
+print "Steps: $steps\n";
